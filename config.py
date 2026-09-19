83f076162319cd3c646f9b7f1cf986433928bb7a
Punsara <hello.punsara@gmail.com>
2026-09-19 07:25:41 +0530

    Fix typo in comment about requesting the .env file

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ wake words, model names, mic settings, or personality text.
 API keys are loaded from a .env file (see .env.example) so you don't
 have to re-export them every terminal session.
 """
-pls request this file,this contain personal information.thanks
+*pls request this file,this contain personal information. thanks
 
 BRAIN_TEMPERATURE = _float_env("BRAIN_TEMPERATURE", 0.25 if OFFLINE_MODE else 0.65)
 BRAIN_TOP_P = float(os.environ.get("BRAIN_TOP_P", "0.90"))
